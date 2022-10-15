a1191317ca43ccf4a8df48b667a45a9a9baa2ab6
itamarbeeri <itamarbeeri2@gmail.com>
2022-10-16 02:11:34 +0300

    semi broken commit.. trying to have infinite line of subtasks

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from colorama import Fore, Back
 
 colorama.init(autoreset=True)
 
-# TODO: get rid of these
 exp = True
 hide_done = True
-task_pointer = None
+task_pointer_list = []
 indent = '    '
 
 class Task:
@@ -79,11 +78,19 @@ def print_instructions():
     sprint('..')
     sprint('Good luck!')
 
+def get_task(Tasks, task_pointer_list):
+    task = Tasks[task_pointer_list[0]]
+    for num in task_pointer_list[1:]:
+        task = task.itemList[num]
+    return task
 
 def display_tasks(Tasks):
     for i, task in enumerate(Tasks):
         task.print(start=' ' + str(i) + '.')
 
+
+def swap_tasks(Tasks, task_pointer_list, dir):
+    pass
 
 def load_tasks(taskfile):
     if not path.isfile(taskfile):
@@ -99,7 +106,7 @@ def save_tasks(taskfile, Tasks):
 
 
 def parse_command(cmd, Tasks):
-    global exp, hide_done, task_pointer
+    global exp, hide_done, task_pointer_list
     cmd = cmd.lower()
     # TODO: fix sub tasks colors
 
@@ -120,13 +127,8 @@ def parse_command(cmd, Tasks):
         isUpdate = True
 
     elif cmd == 'w' or cmd == 's':
-        # TODO: deal with sub tasks
         direction = - 1 if cmd == 'w' else 1
-        if 0 <= task_pointer + direction < len(Tasks):
-            temp = Tasks[task_pointer]
-            Tasks[task_pointer] = Tasks[task_pointer + direction]
-            Tasks[task_pointer + direction] = temp
-            task_pointer = task_pointer + direction
+        task_pointer_list = swap_tasks(Tasks, task_pointer_list, direction)
         isUpdate = True
 
     elif not cmd[0].isnumeric():
@@ -135,57 +137,50 @@ def parse_command(cmd, Tasks):
 
     else:
         cmd_list = cmd.split(' ')
-        task_num = int(cmd_list[0])
-        subtask_num = int(cmd_list[1]) if cmd_list[1][0].isnumeric() else None
-        opcode_offset = 1 if subtask_num is None else 2
-        opcode = cmd_list[opcode_offset]
-        data = ' '.join(cmd_list[opcode_offset + 1:])
-        if len(Tasks) < task_num:
-            sprint(f"{task_num} is an invalid task number for {len(Tasks)} tasks.")
-            isUpdate = False
-        else:
-            isUpdate = execute_command(Tasks, task_num, subtask_num, opcode, data)
+        cmd_offset = 0
+        while cmd_list[cmd_offset].isnumeric():
+            task_pointer_list.append(int(cmd_list[cmd_offset]))
+            cmd_offset += 1
+        opcode = cmd_list[cmd_offset]
+        data = ' '.join(cmd_list[cmd_offset + 1:])
 
+        try:
+            task = get_task(Tasks, task_pointer_list)
+
+        except:
+            sprint(f"ERROR! {task_pointer_list} is an invalid task number!.")
+            isUpdate = False
+
+        else:
+            try:
+                isUpdate = execute_command(Tasks, task_pointer_list, task, opcode, data)
+            except:
+                sprint(f"ERROR! cant execute command.")
+                isUpdate = False
     return isUpdate
 
 
-def execute_command(Tasks, task_num, subtask_num, opcode, data):
-    global task_pointer
+def execute_command(Tasks, task_pointer_list, task, opcode, data):
+
     if opcode == 'del':
-        # TODO: replace the following 4 lines with set attribute: Tasks[task_num].set(att, value, subtask_num)
-        if subtask_num is None:
-            del Tasks[task_num]
-        else:
-            del Tasks[task_num].itemList[subtask_num]
+        del task
         isUpdate = True
 
     elif opcode == 'd':
-        if subtask_num is None:
-            Tasks[task_num].isDone = not Tasks[task_num].isDone
-        else:
-            Tasks[task_num].itemList[subtask_num].isDone = not Tasks[task_num].itemList[subtask_num].isDone
+        task.isDone = not task.isDone
         isUpdate = True
 
     elif opcode == 'e':
-        Tasks[task_num].expendItems = not Tasks[task_num].expendItems
+        task.expendItems = not task.expendItems
         isUpdate = True
 
     elif opcode == 'a':
-        if subtask_num is None:
-            Tasks[task_num].addItem(data)
-        else:
-            Tasks[task_num].itemList[subtask_num].addItem(data)
-
+        task.addItem(data)
         isUpdate = True
 
     elif opcode == 'w' or opcode == 's':
-        # TODO: deal with sub tasks
         direction = - 1 if opcode == 'w' else 1
-        if 0 <= task_num + direction < len(Tasks):
-            temp = Tasks[task_num]
-            Tasks[task_num] = Tasks[task_num + direction]
-            Tasks[task_num + direction] = temp
-            task_pointer = task_num + direction
+        task_pointer_list = swap_tasks(Tasks, task_pointer_list, direction)
         isUpdate = True
 
     elif opcode == 'c':
@@ -204,17 +199,11 @@ def execute_command(Tasks, task_num, subtask_num, opcode, data):
         else:
             color = Fore.YELLOW
 
-        if subtask_num is None:
-            Tasks[task_num].setColor(color)
-        else:
-            Tasks[task_num].itemList[subtask_num].setColor(color)
+        task.setColor(color)
         isUpdate = True
 
     else:
-        if subtask_num is None:
-            Tasks[task_num].name = ' '.join([opcode, data])
-        else:
-            Tasks[task_num].itemList[subtask_num].name = ' '.join([opcode, data])
+        task.name = ' '.join([opcode, data])
         isUpdate = True
 
     return isUpdate
