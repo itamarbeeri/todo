23ec286664eafe454b2c0ad3ea2da80c790e390e
itamarbeeri <itamarbeeri2@gmail.com>
2022-10-15 16:49:49 +0300

    fixed most issues and added save

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import pickle
+from os import path
+
 import colorama
 from colorama import Fore, Back
+
 colorama.init(autoreset=True)
+
 
 class Task:
     def __init__(self, name, isDone=False, color=Fore.YELLOW, expendItems=False):
@@ -12,152 +17,160 @@ class Task:
         self.itemList = []
         self.expendItems = expendItems
 
-    def addItem(self, item):
-        self.itemList.append(item)
+    def addItem(self, name):
+        self.itemList.append(Task(name, color=self.color))
+
+    def setColor(self, color):
+        for item in self.itemList:
+            if self.color == item.color:
+                item.color = color
+        self.color = color
 
     def print(self, start=None):
         bg_color = Back.GREEN if self.isDone is True else Back.BLACK
         start = '' if start is None else str(start)
         print(f"{bg_color}{self.color}{start} {self.name}")
         if self.expendItems is True:
-            for item in self.expendItems:
+            for item in self.itemList:
                 item.print(start='     ')
 
 
-def print_instructions():
-    def sprint(text):
-        print(f"{Fore.LIGHTRED_EX}{text}")
+def sprint(text):
+    print(f"{Fore.LIGHTRED_EX}{text}")
 
+
+def print_instructions():
     sprint('Welcome to TODO list.')
     sprint('..')
 
-    sprint('Create a new task - type n aasdasd where aasdasd is the task name.')
-    sprint('Choose an existing task - type t## where ## is the task number.')
-    sprint('Choose a sub task - after choosing a task, type st## where ## is the subtask number.')
-    sprint('Edit a choosen task:')
-    sprint(' - d to toggle Done/UnDone')
-    sprint(' - e to toggle sub items expantion display')
-    sprint(' - c followed by color to change color - c ,b ,r ,m for cyan, blue, red and magenta. e.g: c c for cyan.')
-    sprint(' - i followed by color to change color.')
-    sprint(' - arrows to move the task up/down in task list')
-    sprint(' - del to delete task.')
-    sprint('Save changes - type s.')
-    sprint('Quit - type q')
+    sprint('Create a new task - type the task name.')
+    sprint('Edit a task - type the task number followed by command')
+    sprint('  d (to toggle Done/UnDone)')
+    sprint('  e (to toggle sub items expantion display)')
+    sprint('  c (change task color) followed by color to change color - c ,b ,r ,m for cyan, blue, red and magenta.')
+    sprint('  a (add sub task) followed by sub task name')
+    sprint('  del to delete task.')
+    sprint("Example: '6 c m' -> color task 6 in magenta. '2 d' toggle task 2 done status")
 
     sprint('..')
-    sprint('Done tasks are colored in green.')
     sprint('Good luck!')
 
-def load_tasks():
-    # TODO: build
-    return []
 
-def save_tasks():
-    # TODO: build
-    return []
-
-taskPointer = None
-subTaskPointer = None
-Tasks = load_tasks()
-
-def main():
-    print(f"{Fore.LIGHTRED_EX} welcome to TODO list:")
-
-    while True:
-        cmd = input()
-        if parse_command(cmd):
-            for i, task in enumerate(Tasks):
-                task.print(start=' ' + str(i) + '. ')
-
-        print(f'{Fore.LIGHTRED_EX} task pointer is: {taskPointer}. sub task pointer is: {subTaskPointer}')
+def display_tasks(Tasks):
+    for i, task in enumerate(Tasks):
+        task.print(start=' ' + str(i) + '. ')
 
 
-def parse_command(cmd):
-    global taskPointer
-    global subTaskPointer
+def load_tasks(taskfile):
+    if not path.isfile(taskfile):
+        return []
+    with open(taskfile, "rb") as fp:
+        Tasks = pickle.load(fp)
+    return Tasks
+
+
+def save_tasks(taskfile, Tasks):
+    with open(taskfile, "wb") as fp:
+        pickle.dump(Tasks, fp)
+
+
+def parse_command(cmd, Tasks):
     cmd = cmd.lower()
-    isUpdate = False
+    # TODO: fix sub tasks colors
 
     if cmd == 'help':
         print_instructions()
         isUpdate = False
 
-    elif cmd.startswith('n'):
-        name = cmd[2:]
-        taskPointer = len(Tasks)
-        subTaskPointer = None
-        Tasks.append(Task(name))
+    elif not cmd[0].isnumeric():
+        Tasks.append(Task(cmd))
         isUpdate = True
 
-    elif cmd.startswith('s'):
-        # TODO: save
-        isUpdate = False
-
-    elif cmd.startswith('q'):
-        # TODO: quit
-        isUpdate = False
-
-    elif cmd.startswith('t') and cmd[1:].isnumeric():
-        num = int(cmd[1:])
-        if len(Tasks) < num:
-            print(f'Task number {num} doesnt exist.')
+    else:
+        cmd_list = cmd.split(' ')
+        task_num = int(cmd_list[0])
+        subtask_num = int(cmd_list[1]) if cmd_list[1][0].isnumeric() else None
+        opcode_offset = 1 if subtask_num is None else 2
+        opcode = cmd_list[opcode_offset]
+        data = ' '.join(cmd_list[opcode_offset + 1:])
+        if len(Tasks) < task_num:
+            sprint(f"{task_num} is an invalid task number for {len(Tasks)} tasks.")
+            isUpdate = False
         else:
-            taskPointer = num
-            subTaskPointer = None
-        isUpdate = False
+            isUpdate = execute_command(Tasks, task_num, subtask_num, opcode, data)
 
-    elif cmd.startswith('st') and cmd[2:].isnumeric():
-        num = int(cmd[2:])
-        if len(Tasks.itemList) < num:
-            print(f'Sub task number {num} for task number {taskPointer} doesnt exist.')
-        else:
-            subTaskPointer = num
-        isUpdate = False
+    return isUpdate
 
-    elif cmd == 'd' and taskPointer is not None:
-        if subTaskPointer is None:
-            Tasks[taskPointer].isDone = not Tasks[taskPointer].isDone
+
+def execute_command(Tasks, task_num, subtask_num, opcode, data):
+    if opcode == 'del':
+        # TODO: replace the following 4 lines with set attribute: Tasks[task_num].set(att, value, subtask_num)
+        if subtask_num is None:
+            del Tasks[task_num]
         else:
-            Tasks[taskPointer].itemList[subTaskPointer].isDone = not Tasks[taskPointer].itemList[subTaskPointer].isDone
+            del Tasks[task_num].itemList[subtask_num]
         isUpdate = True
 
-
-    elif cmd == 'e' and taskPointer is not None:
-        if subTaskPointer is None:
-            Tasks[taskPointer].expendItems = not Tasks[taskPointer].expendItems
+    elif opcode == 'd':
+        if subtask_num is None:
+            Tasks[task_num].isDone = not Tasks[task_num].isDone
+        else:
+            Tasks[task_num].itemList[subtask_num].isDone = not Tasks[task_num].itemList[subtask_num].isDone
         isUpdate = True
 
-    elif cmd.startswith('c') and taskPointer is not None:
-        if cmd[2:].startswith('b'):
+    elif opcode == 'e':
+        Tasks[task_num].expendItems = not Tasks[task_num].expendItems
+        isUpdate = True
+
+    elif opcode == 'a':
+        Tasks[task_num].addItem(data)
+        isUpdate = True
+
+    elif opcode == 'w' or opcode == 's':
+        direction = - 1 if opcode == 'w' else 1
+        if 0 <= task_num + direction < len(Tasks):
+            temp = Tasks[task_num]
+            Tasks[task_num] = Tasks[task_num + direction]
+            Tasks[task_num + direction] = temp
+        isUpdate = True
+
+    elif opcode == 'c':
+        if data.startswith('b'):
             color = Fore.BLUE
-        elif cmd[2:].startswith('r'):
+        elif data.startswith('r'):
             color = Fore.RED
-        elif cmd[2:].startswith('m'):
+        elif data.startswith('m'):
             color = Fore.LIGHTMAGENTA_EX
-        elif cmd[2:].startswith('c'):
+        elif data.startswith('c'):
             color = Fore.CYAN
         else:
             color = Fore.YELLOW
 
-        if subTaskPointer is None:
-            Tasks[taskPointer].color = color
+        if subtask_num is None:
+            Tasks[task_num].setColor(color)
         else:
-            Tasks[taskPointer].itemList[subTaskPointer].color = color
+            Tasks[task_num].itemList[subtask_num].setColor(color)
         isUpdate = True
-
-
-    elif cmd == 'i' and taskPointer is not None:
-        # TODO: add sub item
-        isUpdate = True
-
-    # TODO: arrows
-    # TODO: del
 
     else:
-        print(f'{cmd} is an invalid command.')
+        sprint(f"{opcode} is an invalid operation. try 'help''")
+        isUpdate = False
 
     return isUpdate
 
+
+def main():
+    taskfile = "taskfile"
+    Tasks = load_tasks(taskfile)
+    sprint('welcome to TODO list:')
+    display_tasks(Tasks)
+
+    while True:
+        cmd = input()
+        if parse_command(cmd, Tasks):
+            display_tasks(Tasks)
+        save_tasks(taskfile, Tasks)
+
+
 if __name__ == '__main__':
     main()
-
