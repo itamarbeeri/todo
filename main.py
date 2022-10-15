e09800609b15931b281380c880138a6b6a06fff6
itamarbeeri <itamarbeeri2@gmail.com>
2022-10-16 00:43:39 +0300

    adding features

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ from colorama import Fore, Back
 
 colorama.init(autoreset=True)
 
+# TODO: get rid of these
+exp = True
+hide_done = True
+task_pointer = None
 
 class Task:
-    def __init__(self, name, isDone=False, color=Fore.WHITE, expendItems=True):
+    def __init__(self, name, isDone=False, color=Fore.WHITE, expendItems=True, display=True):
         self.name = name
         self.isDone = isDone
         self.color = color
         self.itemList = []
         self.expendItems = expendItems
+        self.display = display
 
     def addItem(self, name):
         self.itemList.append(Task(name, color=self.color))
@@ -26,13 +31,26 @@ class Task:
                 item.color = color
         self.color = color
 
+    def set_expension(self, val):
+        self.expendItems = val
+        for item in self.itemList:
+            item.set_expension(val)
+
+    def set_display_done(self, val):
+        if self.isDone is True:
+            self.display = val
+        for item in self.itemList:
+            item.set_display_done(val)
+
     def print(self, start=None):
-        bg_color = Back.LIGHTGREEN_EX if self.isDone is True else Back.BLACK
-        start = '' if start is None else str(start)
-        print(f"{bg_color}{self.color}{start} {self.name}")
-        if self.expendItems is True:
-            for item in self.itemList:
-                item.print(start='     ')
+        if self.display is True:
+            bg_color = Back.LIGHTGREEN_EX if self.isDone is True else Back.BLACK
+            start = '' if start is None else str(start)
+            end = '' if self.expendItems is True else ' ...'
+            print(f"{bg_color}{self.color}{start} {self.name}{end}")
+            if self.expendItems is True:
+                for i, item in enumerate(self.itemList):
+                    item.print(start='     ' + str(i) + '.')
 
 
 def sprint(text):
@@ -44,6 +62,8 @@ def print_instructions():
     sprint('..')
 
     sprint('Create a new task - type the task name.')
+    sprint('expand/collapse all press x')
+    sprint('hide/display all done tasks press d')
     sprint('Edit a task - type the task number followed by command')
     sprint('  d (to toggle Done/UnDone)')
     sprint('  e (to toggle sub items expantion display)')
@@ -58,7 +78,7 @@ def print_instructions():
 
 def display_tasks(Tasks):
     for i, task in enumerate(Tasks):
-        task.print(start=' ' + str(i) + '. ')
+        task.print(start=' ' + str(i) + '.')
 
 
 def load_tasks(taskfile):
@@ -75,12 +95,35 @@ def save_tasks(taskfile, Tasks):
 
 
 def parse_command(cmd, Tasks):
+    global exp, hide_done, task_pointer
     cmd = cmd.lower()
     # TODO: fix sub tasks colors
 
     if cmd == 'help':
         print_instructions()
         isUpdate = False
+
+    elif cmd == 'x':
+        exp = not exp
+        for task in Tasks:
+            task.set_expension(exp)
+        isUpdate = True
+
+    elif cmd == 'd':
+        hide_done = not hide_done
+        for task in Tasks:
+            task.set_display_done(hide_done)
+        isUpdate = True
+
+    elif cmd == 'w' or cmd == 's':
+        # TODO: deal with sub tasks
+        direction = - 1 if cmd == 'w' else 1
+        if 0 <= task_pointer + direction < len(Tasks):
+            temp = Tasks[task_pointer]
+            Tasks[task_pointer] = Tasks[task_pointer + direction]
+            Tasks[task_pointer + direction] = temp
+            task_pointer = task_pointer + direction
+        isUpdate = True
 
     elif not cmd[0].isnumeric():
         Tasks.append(Task(cmd))
@@ -103,6 +146,7 @@ def parse_command(cmd, Tasks):
 
 
 def execute_command(Tasks, task_num, subtask_num, opcode, data):
+    global task_pointer
     if opcode == 'del':
         # TODO: replace the following 4 lines with set attribute: Tasks[task_num].set(att, value, subtask_num)
         if subtask_num is None:
@@ -133,6 +177,7 @@ def execute_command(Tasks, task_num, subtask_num, opcode, data):
             temp = Tasks[task_num]
             Tasks[task_num] = Tasks[task_num + direction]
             Tasks[task_num + direction] = temp
+            task_pointer = task_num + direction
         isUpdate = True
 
     elif opcode == 'c':
@@ -159,9 +204,9 @@ def execute_command(Tasks, task_num, subtask_num, opcode, data):
 
     else:
         if subtask_num is None:
-            Tasks[task_num].name = data
+            Tasks[task_num].name = ' '.join([opcode, data])
         else:
-            Tasks[task_num].itemList[subtask_num].name = data
+            Tasks[task_num].itemList[subtask_num].name = ' '.join([opcode, data])
         isUpdate = True
 
     return isUpdate
