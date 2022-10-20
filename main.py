771142600c478e35020ec68546c3b7a0b2d946b3
itamarbeeri <itamarbeeri2@gmail.com>
2022-10-20 19:56:30 +0300

    updated parser

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ from colorama import Fore, Back, Style
 
 colorama.init(autoreset=True)
 
-exp = True
-task_pointer_list = []
 indent = '    '
-hide_done = False
+
 
 def sprint(text):
     print(f"{Fore.LIGHTRED_EX}{text}")
 
 
-def print_instructions():
+def print_instructions(State):
     sprint('Welcome to TODO list.')
+    sprint('Display state is:')
+    sprint(State)
     sprint('..')
     sprint('To create a new task - type the task name.')
     sprint('e to expand/collapse all')
@@ -46,7 +46,7 @@ def print_instructions():
 
 
 class Task:
-    def __init__(self, name, color=Fore.WHITE, expendItems=True, display=True):
+    def __init__(self, name, color=Fore.WHITE, expendItems=True):
         self.name = name
         self.status = {'done': False, 'taken_care_of': False, 'priority': False, 'irrelevant': False}
         self.itemList = []
@@ -98,16 +98,16 @@ class Task:
             fg_color = Fore.WHITE
 
         elif self.status["priority"] is True:
-            if State["mark_priority"] is True:
-                bg_color = Back.LIGHTMAGENTA_EX
-                fg_color = Fore.BLACK + Style.NORMAL
+            if State["mark_priority"] is True and State['priority_only'] is False:
+                bg_color = Back.BLACK
+                fg_color = self.color + Style.DIM
             else:
                 bg_color = Back.BLACK
-                fg_color = self.color
+                fg_color = self.color + Style.NORMAL
 
         else:
             bg_color = Back.BLACK
-            fg_color = self.color
+            fg_color = self.color + Style.BRIGHT
 
         return fg_color, bg_color, visible
 
@@ -116,13 +116,16 @@ class Task:
         fg_color, bg_color, visible = self.get_display_params(State)
         if visible is True:
             start = '' if start is None else str(start)
-            end = '' if self.expendItems is True else ' ...'
+            end = '' if len(self.itemList) == 0 else ' ...'
             print(f"{bg_color}{fg_color}{start} {self.name}{end}")
 
-            if self.expendItems is True:
-                for i, item in enumerate(self.itemList):
+        if self.expendItems is True:
+            for i, item in enumerate(self.itemList):
+                if State['priority_only'] is True:
+                    sub_start = start + str(i) + '.'
+                else:
                     sub_start = ''.join([' ' for _ in range(start.count(' '))]) + indent + str(i) + '.'
-                    item.print(State, start=sub_start)
+                item.print(State, start=sub_start)
 
 
 def get_task(Tasks, task_pointer_list):
@@ -135,6 +138,7 @@ def get_task(Tasks, task_pointer_list):
 def display_tasks(State, Tasks):
     for i, task in enumerate(Tasks):
         task.print(State, start=' ' + str(i) + '.')
+    sprint('---------------------------------------------------------------------------------------------------------')
 
 
 def swap_tasks(Tasks, task_pointer_list, direction):
@@ -174,114 +178,110 @@ def save_data(taskfile, State, Tasks):
         pickle.dump(data, fp)
 
 
-def parse_command(cmd, State, Tasks):
-    global exp, hide_done, task_pointer_list, highlight_only_flag
+def parse_command(cmd):
     cmd = cmd.lower()
-    isUpdate = True
 
-    if cmd == 'help':
-        print_instructions()
+    cmd_list = cmd.split(' ')
+    opcode = next(word for word in cmd_list if not word.isnumeric())
+    cmd_list.remove(opcode)
 
-    elif cmd == 'd':
+    src_pointer = []
+    cmd_offset = 0
+    if len(cmd_list) == 0:
+        data = ''
+    else:
+        while cmd_offset < len (cmd_list) and cmd_list[cmd_offset].isnumeric():
+            src_pointer.append(int(cmd_list[cmd_offset]))
+            cmd_offset += 1
+        data = ' '.join(cmd_list[cmd_offset:])
+    src_pointer = '' if len(src_pointer) == 0 else src_pointer
+    cmd_dict = {'opcode': opcode, 'src_pointer': src_pointer, 'data': data}
+
+    return cmd_dict
+
+
+def execute_command_general(cmd_dict, State, Tasks):
+    if cmd_dict['opcode'] == 'help':
+        print_instructions(State)
+
+    elif cmd_dict['opcode'] == 'd':
         State['display_done'] = not State['display_done']
 
-    elif cmd == 'f':
+    elif cmd_dict['opcode'] == 'f':
         State['display_irrelevant'] = not State['display_irrelevant']
 
-    elif cmd == 'g':
+    elif cmd_dict['opcode'] == 'g':
         State['display_taken_care_of'] = not State['display_taken_care_of']
 
-    elif cmd == 'h':
+    elif cmd_dict['opcode'] == 'h':
         State['priority_only'] = not State['priority_only']
 
-    elif cmd == 'hh':
+    elif cmd_dict['opcode'] == 'hh':
         State['mark_priority'] = not State['mark_priority']
 
-    elif cmd == 'e':
+    elif cmd_dict['opcode'] == 'e':
         State['expand_all'] = not State['expand_all']
         for task in Tasks:
             task.set_expension(State['expand_all'])
 
-    elif cmd == 'w' or cmd == 's':
-        direction = - 1 if cmd == 'w' else 1
-        task_pointer_list = swap_tasks(Tasks, task_pointer_list, direction)
-
-    elif not cmd[0].isnumeric():
-        Tasks.append(Task(cmd))
+    elif cmd_dict['opcode'] == 'w' or cmd_dict['opcode'] == 's':
+        direction = - 1 if cmd_dict['opcode'] == 'w' else 1
+        cmd_dict['src_pointer'] = swap_tasks(Tasks, cmd_dict['src_pointer'], direction)
 
     else:
-        cmd_list = cmd.split(' ')
-        cmd_offset = 0
-        task_pointer_list = []
-        while cmd_list[cmd_offset].isnumeric():
-            task_pointer_list.append(int(cmd_list[cmd_offset]))
-            cmd_offset += 1
-        opcode = cmd_list[cmd_offset]
-        data = ' '.join(cmd_list[cmd_offset + 1:])
-
-        try:
-            task = get_task(Tasks, task_pointer_list)
-
-        except:
-            sprint(f"ERROR! {task_pointer_list} is an invalid task number!.")
-            isUpdate = False
-
-        else:
-            try:
-                execute_command(Tasks, task_pointer_list, task, opcode, data)
-            except:
-                sprint(f"ERROR! cant execute command.")
-                isUpdate = False
-    return isUpdate
+        new_task = ' '.join([cmd_dict['opcode'], cmd_dict['src_pointer'], cmd_dict['data']])
+        Tasks.append(Task(new_task))
 
 
-def execute_command(Tasks, task_pointer_list, task, opcode, data):
-    if opcode == 'del':
-        pointer = task_pointer_list[-1]
-        if len(task_pointer_list) == 1:
+def execute_command_specific(cmd_dict, Tasks):
+    task = get_task(Tasks, cmd_dict['src_pointer'])
+
+    if cmd_dict['opcode'] == 'del':
+        pointer = cmd_dict['src_pointer'][-1]
+        if len(cmd_dict['src_pointer']) == 1:
             del Tasks[pointer]
         else:
-            parent_task = get_task(Tasks, task_pointer_list[:-1])
+            parent_task = get_task(Tasks, cmd_dict['src_pointer'][:-1])
             del parent_task.itemList[pointer]
 
-    elif opcode == 'd':
+    elif cmd_dict['opcode'] == 'd':
         task.status['done'] = not task.status['done']
 
-    elif opcode == 'e':
+    elif cmd_dict['opcode'] == 'e':
         task.expendItems = not task.expendItems
 
-    elif opcode == 'a':
-        task.add_item(data)
+    elif cmd_dict['opcode'] == 'a':
+        task.add_item(cmd_dict['data'])
 
-    elif opcode == 'h':
+    elif cmd_dict['opcode'] == 'h':
         task.status['priority'] = not task.status['priority']
 
-    elif opcode == 'g':
+    elif cmd_dict['opcode'] == 'g':
         task.status['taken_care_of'] = not task.status['taken_care_of']
 
-    elif opcode == 'f':
+    elif cmd_dict['opcode'] == 'f':
         task.status['irrelevant'] = not task.status['irrelevant']
 
-    elif opcode == 'w' or opcode == 's':
-        direction = - 1 if opcode == 'w' else 1
-        task_pointer_list = swap_tasks(Tasks, task_pointer_list, direction)
+    elif cmd_dict['opcode'] == 'w' or cmd_dict['opcode'] == 's':
+        direction = - 1 if cmd_dict['opcode'] == 'w' else 1
+        cmd_dict['src_pointer'] = swap_tasks(Tasks, cmd_dict['src_pointer'], direction)
 
-    elif opcode == 'c':
-        if data.startswith('b'):
+    elif cmd_dict['opcode'] == 'c':
+        if cmd_dict['data'].startswith('b'):
             color = Fore.BLUE
-        elif data.startswith('r'):
+        elif cmd_dict['data'].startswith('r'):
             color = Fore.RED
-        elif data.startswith('m'):
+        elif cmd_dict['data'].startswith('m'):
             color = Fore.LIGHTMAGENTA_EX
-        elif data.startswith('c'):
+        elif cmd_dict['data'].startswith('c'):
             color = Fore.CYAN
-        elif data.startswith('y'):
+        elif cmd_dict['data'].startswith('y'):
             color = Fore.YELLOW
-        elif data.startswith('g'):
+        elif cmd_dict['data'].startswith('g'):
             color = Fore.GREEN
-        elif data.startswith('w'):
+        elif cmd_dict['data'].startswith('w'):
             color = Fore.WHITE
-        elif data.startswith('k'):
+        elif cmd_dict['data'].startswith('k'):
             color = Fore.BLACK
         else:
             color = Fore.WHITE
@@ -289,7 +289,7 @@ def execute_command(Tasks, task_pointer_list, task, opcode, data):
         task.set_color(color)
 
     else:
-        task.name = ' '.join([opcode, data])
+        task.name = ' '.join([cmd_dict['opcode'], cmd_dict['data']])
 
 
 def main():
@@ -300,8 +300,14 @@ def main():
 
     while True:
         cmd = input()
-        if parse_command(cmd, State, Tasks):
-            display_tasks(State, Tasks)
+        cmd_dict = parse_command(cmd)
+
+        if len(cmd_dict['src_pointer']) == 0:
+            execute_command_general(cmd_dict, State, Tasks)
+        else:
+            execute_command_specific(cmd_dict, Tasks)
+
+        display_tasks(State, Tasks)
         save_data(taskfile, State, Tasks)
 
 
